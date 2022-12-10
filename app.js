ce68d6a3cd6d4aa2c5b7e249d03d3e5c9640749e
Tyler Dickens <82357550+tdickens777@users.noreply.github.com>
2022-12-10 00:40:58 -0800

    Add to page

--- app.js
+++ app.js
@@ -7,7 +7,31 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCrsmDHBCqXRl_2NOF1f3oFfCXvrMWdoR0",
+    authDomain: "portfoliopage-bf160.firebaseapp.com",
+    projectId: "portfoliopage-bf160",
+    storageBucket: "portfoliopage-bf160.appspot.com",
+    messagingSenderId: "911510036212",
+    appId: "1:911510036212:web:da22a4e45de26c0d8788a1",
+    measurementId: "G-H5W7DDK9VL"
+};
+
+// Initialize Firebase
+const fireApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireApp);
+
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
